0bc627a5813a40244e407dc6c0eb841712efd75c
Joshua Rubin <joshuarubin@users.noreply.github.com>
2014-10-28 18:01:44 -0600

    Create osx.plugin.zsh

---
+++ osx.plugin.zsh
@@ -0,0 +1,25 @@
+#
+# Defines Mac OS X aliases and functions.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Return if requirements are not found.
+if [[ "$OSTYPE" != darwin* ]]; then
+  return 1
+fi
+
+#
+# Aliases
+#
+
+# Changes directory to the current Finder directory.
+alias cdf='cd "$(pfd)"'
+
+# Pushes directory to the current Finder directory.
+alias pushdf='pushd "$(pfd)"'
+
+local fdir=$0:A:h/functions
+fpath+=$fdir
+autoload -Uz $fdir/*(:t)
